f8bcf9b0223dd685aeb6d463206b84432e83f1c0
ikangethe <ianngech@gmail.com>
2022-09-06 20:58:56 +0300

    added dependencies

---
+++ src/main/java/com/loaning/loanrepay/LoanRepayApplication.java
@@ -0,0 +1,13 @@
+package com.loaning.loanrepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanRepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanRepayApplication.class, args);
+	}
+
+}
